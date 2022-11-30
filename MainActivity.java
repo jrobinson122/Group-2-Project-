3d943d2ed1e4ef948103f13f9fe22d33d81a29e4
jrobinson122 <103521939+jrobinson122@users.noreply.github.com>
2022-11-30 14:02:44 -0500

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,5 +1,18 @@
 package com.example.safesoundfinalproject;
 
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.media.AudioRecord;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -12,10 +25,13 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private AudioRecord ar = null;
+    private int minSize;
     private SensorManager mSensorManager;
     private Sensor mSensor;
     private TextView mTextView;
-
     private SensorEventListener mSensorListener = new SensorEventListener() {
 
         @Override
@@ -26,28 +42,56 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onAccuracyChanged(Sensor sensor, int accuracy) {
         }
+
     };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        minSize = AudioRecord.getMinBufferSize(8000, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
 
         mTextView = (TextView) findViewById(R.id.textView);
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        mSensor = mSensorManager.getDefaultSensor(minSize);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        ar = new AudioRecord(MediaRecorder.AudioSource.MIC, 8000, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, minSize);
+        ar.startRecording();
     }
 
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
         mSensorManager.registerListener(mSensorListener, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
 
     }
 
     @Override
-    protected void onPause(){
+    protected void onPause() {
         super.onPause();
         mSensorManager.unregisterListener(mSensorListener);
+        if (ar != null) {
+            ar.stop();
+        }
     }
-}
+        public double getAmplitude() {
+            short[] buffer = new short[minSize];
+            ar.read(buffer, 0, minSize);
+            int max = 0;
+            for (short s : buffer) {
+                if (Math.abs(s) > max) {
+                    max = Math.abs(s);
+                }
+            }
+            return max;
+        }
+    }
